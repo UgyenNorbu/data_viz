9d90c8c7bd07cfb7ebfba025c1c09f3471cef328
Ugyen Norbu <66914661+UgyenNorbu@users.noreply.github.com>
2020-08-26 10:47:58 +0600

    2nd chapter

--- model.R
+++ model.R
@@ -22,3 +22,26 @@ gapminder %>%
     geom_point(alpha = 0.3) +
     geom_quantile(color = "tomato", size = 1.2, method = "rqss",
                   lambda = 1, quantiles = c(0.20, 0.5, 0.85))
+
+mod_color <- RColorBrewer::brewer.pal(3, "Set1")
+
+gapminder %>% 
+    ggplot(aes(x = log(gdpPercap), y = lifeExp)) +
+    geom_point(alpha = 0.3) +
+    geom_smooth(method = "lm", aes(color = "OLS", fill = "OLS")) + 
+    geom_smooth(method = "lm", formula = y ~ splines::bs(x, df = 3),
+                aes(color = "Cubic Spline", fill = "Cubic Spline")) + 
+    geom_smooth(method = "loess",
+                aes(color = "LOESS", fill = "LOESS")) +
+    scale_color_manual(name = "Models", values = mod_color) + 
+    scale_fill_manual(name = "Models", values = mod_color) + 
+    theme(legend.position = "top")
+
+    
+    
+    
+    
+    
+    
+    
+
